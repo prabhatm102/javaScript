7c30ca6c8fc894a72dd5e4f16baeadcc488309a6
prabhatm102 <prabhat@rudrainnovative.in>
2021-11-09 18:07:27 +0530

    updated dir node1.0

---
+++ node1.0/app.js
@@ -0,0 +1,6 @@
+const mypkg=require("calc102");
+
+console.log(mypkg.add(7,73));
+console.log(mypkg.sub(121,89));
+console.log(mypkg.mult(9,19));
+console.log(mypkg.div(177,7));
---
+++ node1.0/index.js
@@ -0,0 +1,8 @@
+const { max } = require("underscore");
+const _=require("underscore");
+// check either it is core module or not
+  // then check  it is file or directory
+      // then check inside node_modules directory
+
+console.log(_.contains([23,44,2,54],2));     
+console.log(max([5,6,8,23,54])); 
